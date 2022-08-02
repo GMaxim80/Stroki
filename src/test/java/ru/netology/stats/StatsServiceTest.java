5a75ce04e0920d6fa0a366e45515b5677df70096
GMaxim80 <mgubanov80@gmail.com>
2022-08-02 18:40:43 +0300

    First commit

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,74 @@
+package ru.netology.stats;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test
+    public void test() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.average(sales);
+        long expected = 15;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test3() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.avgLess(sales);
+        long expected = 5;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test4() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.minSales(sales);
+        long expected = 9;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test5() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.maxSales(sales);
+        long expected = 8;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test6() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.avgOver(sales);
+        long expected = 5;
+
+        assertEquals(expected, actual);
+    }
+}
